a958a2cacbf719b4378e8f1a1317cb0129e4d3ee
suryadi445 <suryadi.hhb@gmail.com>
2021-03-23 16:31:53 +0700

    menambahkan fontawesome dan card pada index.php

---
+++ application/views/home/index.php
@@ -0,0 +1,38 @@
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h2 class="text-center mt-3">Movies</h2>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+
+            <div class="card-deck">
+                <div class="card">
+                    <img src="<?= base_url() ?>assets/image/komputer.jpeg">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text">
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="<?= base_url() ?>assets/image/komputer.jpeg">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                        <p class="card-text">
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="<?= base_url() ?>assets/image/komputer.jpeg">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                        <p class="card-text">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
